7cba406358bbef9761b83d2ec014193a473959f3
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-11-17 18:45:09 -0300

    finaliza requisito 6

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,36 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('testa se fetchProduct é uma função', () => {
+
+    expect(typeof fetchProduct).toBe('function');
+  
   });
+
+  it('executa a função fetchProduct e verifica se o fetch é chamado', () => {
+    
+    fetchProduct('MLB1405519561');
+    expect(fetch).toBeCalled();
+
+  });
+
+  it('testa se fetch é chamado com o endpoint correto ao executar fetchProduct', () => {
+
+    fetchProduct('MLB1405519561')
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1405519561')
+
+  });
+
+  it('testa se retorno de fetchProduct é igual ao objeto product', async () => {
+ 
+    await expect(fetchProduct('MLB1405519561')).resolves.toEqual(product);
+
+  });
+
+  it('testa se fetchProduct sem argumento retorna um erro', async () => {
+
+    await expect(fetchProduct()).rejects.toThrow('ID não informado');
+
+  });
+
 });
