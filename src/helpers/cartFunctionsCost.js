aac08b01d14c73e268cc3ca2f49e77a3b8a277bc
Bárbara <babivaldez@gmail.com>
2023-01-20 08:50:52 -0300

    realiza testes na página do carrinho de compras com o toFixed()

--- src/helpers/cartFunctionsCost.js
+++ src/helpers/cartFunctionsCost.js
@@ -6,6 +6,7 @@ const totalPrice = document.querySelector('.total-price'); // span que está den
 
 export function getSavedCartPrice() {
   const cartPrice = localStorage.getItem('cartTotalPrice');
+  console.log(cartPrice);
   if (!cartPrice) {
     return '0';
   }
@@ -26,13 +27,16 @@ export async function decreaseCartPrice(id) {
   const { price } = productSelected;
   // console.log('price: ', price);
   const priceLocalStorage = getSavedCartPrice();
-  let newPriceLocalStorage = priceLocalStorage - price;
+  // console.log('priceLocalStorage:', priceLocalStorage);
+  let newPriceLocalStorage = (priceLocalStorage - price);
   // console.log('newPriceLocalStorage1', newPriceLocalStorage);
   if (newPriceLocalStorage < 0) {
     newPriceLocalStorage = 0;
   }
+  // console.log(totalPrice);
   // console.log('newPriceLocalStorage2', newPriceLocalStorage);
   totalPrice.innerText = newPriceLocalStorage.toString();
+  // o toFixed() deu problema no teste da Trybe, porém não quebra a aplicação no navegador e evita que a subtração do carrinho fique com números decimais em forma de dízima.
   // totalPrice.innerText = newPriceLocalStorage.toFixed(2).toString();
   localStorage.setItem('cartTotalPrice', JSON.stringify(newPriceLocalStorage));
 }
